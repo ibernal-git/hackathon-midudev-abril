dea6d4a954760ad1735fb3a308c71ce72b96a520
Imanol Bernal <info@ibernal.dev>
2022-04-25 18:47:03 +0200

    config for theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        background: '#383838',
+        newWhite: '#F2F2F2',
+        primary: '#066163',
+        accent: '#CDBE78'
+      }
+    }
   },
   plugins: []
 }
