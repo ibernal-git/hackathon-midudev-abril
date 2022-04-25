1b0d7a2ff049b5aa927ee236008d4952315419b6
Imanol Bernal <info@ibernal.dev>
2022-04-25 18:48:23 +0200

    add styles to welcome page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import type { GetServerSideProps, NextPage } from 'next'
 import { getSession, signIn, useSession } from 'next-auth/react'
 import NextHead from 'next/head'
 import VideoChat from '../components/VideoChat'
+import LoginButton from '../components/LogginButton'
+import Image from 'next/image'
 
 const Home: NextPage = () => {
   const { data: session } = useSession()
@@ -16,11 +18,25 @@ const Home: NextPage = () => {
   }
 
   return (
-    <div>
+    <>
       <Head />
+      <div className='flex h-screen items-center justify-center space-between bg-background'>
+        <div className='h-96 mx-auto w-96 bg-primary p-10 rounded text-center shadow-md'>
+
+          <div className='flex flex-col justify-center items-center space-y-4 h-full'>
+          <Image className='' src={'/logo.png'} alt='DEV MEET' width='200px' height='200px' priority layout='fixed' />
+          <h2 className='text-newWhite'>Video Chat App for developers</h2>
+            <div>
+              <LoginButton provider='github'>
+                LOG IN
+              </LoginButton>
+            </div>
+          </div>
+        </div>
+      </div>
       <h1>LOG IN</h1>
       <button onClick={() => signIn('github')}>Log</button>
-    </div>
+    </>
   )
 }
 
@@ -40,7 +56,7 @@ const Head: NextPage = () => {
     <NextHead>
         <title>Dev meet</title>
         <meta name="description" content="Video chat App for developers" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" href="/logo.png" />
       </NextHead>
   )
 }
